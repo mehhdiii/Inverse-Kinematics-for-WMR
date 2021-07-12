4019d5caad15630bd3589ee0163cfe35741ff169
Mehdi <60067141+mehhdiii@users.noreply.github.com>
2021-07-12 15:23:18 +0500

    all results added

--- tracker.m
+++ tracker.m
@@ -1,41 +1,78 @@
 % Differential drive WMR
-
-T = 0.05; %Sampling time
+close all; clc;
+T = 0.001; %Sampling time
 W = 1/2; %m. Width of Robot
 r = 1/4; %m. Radius of tires
 
 % Create Trajectory for WMR:
-N=100;t=linspace(-2*pi,2*pi,N);
-f1=1;x=5*sin(f1*t);f2=2;y=5*sin(f2*t);
-phi = tan(y./x);
-%initialize resulting velocities
+N=400;t=linspace(-pi,pi,N);
+f1=1;x=8*sin(f1*t);f2=2;y=8*sin(f2*t);
+phi =zeros(1, N);
+
+%initialize Inverse kinematics velocities
 omega = zeros(1, N); v = zeros(1, N); 
+vL = zeros(1, N); vR = zeros(1, N); 
 
 %initialize resulting forward kinematic variables: 
 x_f = zeros(1, N); y_f = zeros(1, N); phi_f = zeros(1, N); 
-for n = 1:N-1
+
+%plotting initializations: 
+axis tight manual % this ensures that getframe() returns a consistent size
+filename = 'testAnimated.gif';
+h = figure; 
+
+for n = 2:N-1
+    if n>1
+        phi(n) = atan2(y(n)-y(n-1), x(n)-x(n-1)); 
+    end
     
     %calculating inverse kinematics variables: 
-    mu = 1/2*(sin(phi(n))*(y(n+1)-y(n))+cos(phi(n)*(x(n+1)-x(n))))/(cos(phi(n))*(y(n+1)-y(n))-sin(phi(n))*(x(n+1)-x(n))); 
-    x_m = (x(n)+x(n+1))/2; y_m = (y(n)+y(n+1))/2; 
-    x_star = x_m - mu/2 * (y(n+1) - y(n)); y_star = y_m + mu/2 * (x(n+1)-x(n)); 
+    mu = 1/2*(sin(phi(n))*(y(n+1)-y(n))+cos(phi(n)*(x(n+1)-x(n))))...
+        /(cos(phi(n))*(y(n+1)-y(n))-sin(phi(n))*(x(n+1)-x(n))); 
+    x_m = (x(n)+x(n+1))/2; 
+    y_m = (y(n)+y(n+1))/2; 
+    
+    x_star = x_m - mu/2 * (y(n+1) - y(n)); 
+    y_star = y_m + mu/2 * (x(n+1)-x(n)); 
+    
     R_n = sqrt((x(n) - x_star)^2 + (y(n)-y_star)^2); 
-    theta_1 = atan2((y(n)-y_star), (x(n)-x_star)); theta_2 = atan2((y(n+1)-y_star), (x(n+1)-x_star)); 
-    del_phi = theta_1 - theta_2; 
-    %resulting velocities: 
+    theta_1 = atan2((y(n)-y_star), (x(n)-x_star)); 
+    theta_2 = atan2((y(n+1)-y_star), (x(n+1)-x_star)); 
+    del_phi = wrapToPi(theta_1 - theta_2); 
+    
+    %resulting Inv-Kinematics velocities: 
     omega(n) = del_phi/T; 
-    v(n) = R_n*omega(n);  
+    v(n) = R_n*abs(omega(n));  
+    vL = (R_n-1/2 *W)*omega(n); 
+    vR = (R_n+1/2 *W)*omega(n); 
     
     %forward Kinematics: circular velocity motion model
-    if (n>1)
+    x_f(n+1) = x_f(n) + (v(n)/omega(n))*(-sin(phi(n))+sin(phi(n)+omega(n)*T));
+    y_f(n+1) = y_f(n) + (v(n)/omega(n))*(cos(phi(n))-cos(phi(n)+omega(n)*T));
+    
+    
         
         
-        x_f(n) = x_f(n-1) + (v(n)/omega(n)) * (-sin(phi_f(n-1))+sin(phi_f(n-1)+omega(n)*T)); 
-        y_f(n) = y_f(n-1) + (v(n)/omega(n)) * (cos(phi_f(n-1))-cos(phi_f(n-1)+omega(n)*T)); 
-        phi_f(n) = phi_f(n-1) + omega(n)*T; 
+%     x_f(n) = x_f(n-1) + abs(v(n)/omega(n)) * (-sin(phi(n-1))+sin(phi(n-1)+omega(n)*T)); 
+%     y_f(n) = y_f(n-1) + abs(v(n)/omega(n)) * (cos(phi(n-1))-cos(phi(n-1)+omega(n)*T)); 
+%         phi_f(n) = phi_f(n-1) + omega(n)*T; 
         
-    end
-   
+    
+
+%     figure(1);clf;
+    plot(x_f(1:n), y_f(1:n), 'm-', 'linewidth', 6); 
+    hold on; plot(x, y, 'k-', 'linewidth', 1); drawnow;  
+    
+    %create GIF
+    frame = getframe(h); 
+    im = frame2im(frame); 
+    [imind,cm] = rgb2ind(im,256); 
+    % Write to the GIF File 
+    if n == 2
+      imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+    else 
+      imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+    end 
 end
 
 figure()
@@ -48,7 +85,7 @@ hold off
 figure()
 hold on 
 plot(x,y,'linewidth',2); 
-plot(x_f, y_f, 'linewidth', 2); 
+plot(x_f, y_f, 'linewidth', 6); 
 legend('Original Path', 'Calculated Path')
 hold off
 
